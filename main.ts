1266bbd227983b5b6e0376f818c4ad7000b33232
ReemOthman1 <72812388+ReemOthman1@users.noreply.github.com>
2020-11-29 20:37:58 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 while (true) {
     console.log("tempature" + input.temperature(TemperatureUnit.Fahrenheit))
-    if (input.temperature(TemperatureUnit.Fahrenheit) > 85) {
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 80) {
         light.setAll(light.rgb(255, 0, 0))
     } else if (input.temperature(TemperatureUnit.Fahrenheit) > 40) {
         light.setAll(light.rgb(0, 255, 0))
